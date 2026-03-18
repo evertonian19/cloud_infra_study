1c5413df0c66c966ba2572d5ac23c1a92edc7a38
jacob <wogur2590@naver.com>
2026-03-18 15:54:07 +0900

    4pm

---
+++ Step03_Example.py
@@ -0,0 +1,31 @@
+# Step03_Example.py
+
+"""
+회원 한명의 정보는 tel, name, adr 로 이루어져있다
+그리고 그러한 회원이 여러명이다
+여러명의 회원 정보를
+
+"""
+
+
+#dict 3개를 list에다가 담기
+
+# 딕셔너리 5개를 담은 리스트 'members'
+
+mem1 = {"num":1, "name":"김구라", "addr":"노량진"}
+mem2 = {"num":2, "name":"해골", "addr":"노량진"}
+mem3 = {"num":3, "name":"원숭이", "addr":"동물원"}
+# dict 3 개를 list 에 담기
+members = [mem1, mem2, mem3]
+
+a = members #dict
+b = members[0] #list
+c = members[0]["num"] #dict의 num값
+d = members[2]["name"]
+
+print("clear")
+print("clear")
+print("clear")
+print("clear")
+print("clear")
+print("clear")
---
+++ Step04_Str.py
@@ -0,0 +1,20 @@
+#공백 제거 Str
+
+text = "           Cloud Infra            "
+result1 = text.strip()
+
+
+#.으러 븐리하기
+myIp = "192.168.0.2"
+result2 = myIp.split(".")
+
+#join 다시 합치기
+result3 = ".".join(result2)
+
+#특정 문자열 찾아서 대체하기
+result4 = "hello mimi!".replace("mi","ma", 1)
+
+result5 = "python".upper()
+result6 = "PYTHON".lower()
+
+print("Clear")
