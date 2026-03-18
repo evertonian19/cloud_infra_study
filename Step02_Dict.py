f939cea4de42addae4cb77319e324f986df4b14b
jacob <wogur2590@naver.com>
2026-03-18 12:27:29 +0900

    hello, step01, step02 add

---
+++ Step02_Dict.py
@@ -0,0 +1,24 @@
+# dict type 에 대해서 알아보자!!
+
+'''
+
+  -dict type
+  1. key:value 형태로 데이터를 저장
+  2. 순서가 없는 데이터
+  3. key값을 이용해서 저장된 값을 참조한다
+
+'''
+mem4 = {"name":"son","age":27,"isMan":True}
+
+# 특정 key 값으로 저장된 내용 삭제
+del mem4["isMan"]
+
+mem4.clear()
+
+
+print("Clear")
+
+'''
+func develope asod aff
+naskfkaf akfaf
+'''
