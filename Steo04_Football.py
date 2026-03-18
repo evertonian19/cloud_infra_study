7a5bb5f3baad598bce016ff32e838b0c6edbd0d3
jacob <wogur2590@naver.com>
2026-03-18 12:43:34 +0900

    leauue

--- Steo04_Football.py
+++ Steo04_Football.py
@@ -3,3 +3,10 @@ tottenham = "champiosnship"
 liverfool = "winner"
 
 Everton = "Semi Winner"
+
+#2026 England Premier League Winner
+#1. Liverfool
+#2. Mancity
+#3. Man.Utd
+#4. Arsenal
+#5. Everton
